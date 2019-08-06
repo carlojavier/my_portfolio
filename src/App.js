bc8683a97ba2d40d1a642c03d64eb7f943aa2428
carlo javier <carlomjavier@gmail.com>
2019-08-05 21:39:07 -0700

    practice with counter exercise, set up routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import './App.css';
 
+const HomeScreen = () => {
+  return (
+    <div>
+      <img src={""} />
+      <h1>Welcome to my portfolio</h1>
+    </div>
+  )
+}
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path="/" exact component={HomeScreen} />
+      </Router>
+
     </div>
   );
 }
