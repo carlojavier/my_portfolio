edb6cffd675381a519aa3674f1bd7531f9fff192
carlo javier <carlomjavier@gmail.com>
2019-08-06 18:09:31 -0700

    add about

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,9 +3,21 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to my portfolio</h1>
+    <div className="main-container">
+      <div className="about-title">
+      <h1>about me</h1>
+      </div>
+      <div className="about-container">
+        <p>Hello, my name is Carlo, welcome to my website.
+          I just recently started this with React. Hopefully,
+          it groes into something nice. I'm thinking of 
+          combining my project and writing work together
+          into one comprehensive portfolio. We'll
+          see how it goes... I guess.
+        </p>
+      </div>
     </div>
+
   )
 }
 
